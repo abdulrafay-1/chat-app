043487f0af9f372134146b3ff062700ff5ad1263
abdulrafay-1 <130471410+abdulrafay-1@users.noreply.github.com>
2023-11-26 16:00:15 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC9p9vGYmplbjEtv223PU7bCrbbKuoS4Og",
+    authDomain: "chat-app-64a7a.firebaseapp.com",
+    projectId: "chat-app-64a7a",
+    storageBucket: "chat-app-64a7a.appspot.com",
+    messagingSenderId: "974838052540",
+    appId: "1:974838052540:web:5e56938d6b43ad210dc2b9"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const loginwithgoogle = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user);
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+}
+
+const googleLogin = document.getElementById("loginwithgoogle");
+googleLogin.addEventListener("click", loginwithgoogle);
