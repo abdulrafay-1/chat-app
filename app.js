b0c73e0ffa0887c4c85eaa29e5c592ae0d45bc5d
abdulrafay-1 <130471410+abdulrafay-1@users.noreply.github.com>
2023-12-03 09:38:43 +0000

    onAuthStateChanged, realtime listener

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { signInWithPopup, GoogleAuthProvider, getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { signInWithPopup, GoogleAuthProvider, getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,12 +7,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC9p9vGYmplbjEtv223PU7bCrbbKuoS4Og",
-    authDomain: "chat-app-64a7a.firebaseapp.com",
-    projectId: "chat-app-64a7a",
-    storageBucket: "chat-app-64a7a.appspot.com",
-    messagingSenderId: "974838052540",
-    appId: "1:974838052540:web:5e56938d6b43ad210dc2b9"
+  apiKey: "AIzaSyC9p9vGYmplbjEtv223PU7bCrbbKuoS4Og",
+  authDomain: "chat-app-64a7a.firebaseapp.com",
+  projectId: "chat-app-64a7a",
+  storageBucket: "chat-app-64a7a.appspot.com",
+  messagingSenderId: "974838052540",
+  appId: "1:974838052540:web:5e56938d6b43ad210dc2b9"
 };
 
 
@@ -22,28 +22,40 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const loginwithgoogle = (e) => {
-    e.preventDefault();
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user);
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
+  e.preventDefault();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 }
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 
 const googleLogin = document.getElementById("loginwithgoogle");
 googleLogin.addEventListener("click", loginwithgoogle);
